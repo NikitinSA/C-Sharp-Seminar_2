a2d5a3bd8a333ff053a653044a86815104b68484
Савелий <Savka9765@yandex.ru>
2022-08-08 20:41:56 +0300

    Задача 1. Программа, которая выводит случайное числоиз отрезка от 10 до 99 и показывает наибольшую цифру в числе.

---
+++ Задача 1/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая выводит случайное число из отрезка [10, 99] и
+показывает наибольшую цифру числа.*/
+
+int GetRandomNumberInRange(int min_number, int max_number)
+{
+    int result = new Random().Next(min_number,max_number + 1);
+    return result;
+}
+
+int GetMaxDigitFromNumber(int number)
+{
+    int result = number / 10;
+    int second_digit = number % 10;
+    if(second_digit > result)
+    {
+        result = second_digit;
+    }
+    return result;
+}
+
+int random_number = GetRandomNumberInRange(10, 99);
+int max_digit = GetMaxDigitFromNumber(random_number);
+Console.Write("Наибольшая цифра числа " + random_number + " является " + max_digit);
